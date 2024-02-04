61bfb1fb2b9e8706d2b7de77edace99d37ac7838
Saya <138538771+SayaOvO@users.noreply.github.com>
2024-02-04 20:09:37 +0800

    Time: 65 ms (23.81%) | Memory: 51 MB (26.86%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,6 +1,6 @@
 function removeElement(nums: number[], val: number): number {
     let k = 0;
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0, len = nums.length; i < len; i++) {
         if (nums[i] !== val) {
             nums[k++] = nums[i]
         }
