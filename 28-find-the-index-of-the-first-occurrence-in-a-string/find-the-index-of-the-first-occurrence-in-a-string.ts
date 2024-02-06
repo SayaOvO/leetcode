4ca73a9006830fd48f6393bd3c33a583b5d67642
Saya <138538771+SayaOvO@users.noreply.github.com>
2024-02-06 22:59:06 +0800

    Time: 67 ms (17.57%) | Memory: 50.7 MB (18.75%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,3 +1,17 @@
 function strStr(haystack: string, needle: string): number {
-   return haystack.indexOf(needle); 
+    let needleLen = needle.length;
+    for (let i = 0, len = haystack.length; i < len; i++) {
+        let j = 0;
+        while (j < needleLen) {
+            if (haystack[i+j] !== needle[j]) {
+                break;
+            }
+            j++;
+        }
+        if (j === needleLen) {
+            return i;
+        }
+    }
+    return -1;
+    
 };
