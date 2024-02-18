eb8b8f079def9c86c554b7db6116ff118162c5d0
Saya <138538771+SayaOvO@users.noreply.github.com>
2024-02-18 17:32:28 +0800

    Time: 60 ms (29.98%) | Memory: 50.3 MB (24.78%) - LeetSync

--- 228-summary-ranges/summary-ranges.ts
+++ 228-summary-ranges/summary-ranges.ts
@@ -5,10 +5,11 @@ function summaryRanges(nums: number[]): string[] {
     let i = 0;
     for (let j = 0, len = nums.length; j < len; j++) {
         if (nums[j] !== nums[j+1] - 1) {
-            ranges.push([nums[i], nums[j]])
+            ranges.push(nums[i] === nums[j] ? 
+                `${nums[i]}` : `${nums[i]}->${nums[j]}`
+            )
             i = j + 1;
         }
     }
-    return ranges.map(([i, j]) => i === j ? `${i}` : `${i}->${j}`)
-
+    return ranges;
 };
