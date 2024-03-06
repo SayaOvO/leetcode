1ec2e2848ee191475a5bd9831fcbb57689c88199
Saya <138538771+SayaOvO@users.noreply.github.com>
2024-03-06 22:43:49 +0800

    Time: 66 ms (7.30%) | Memory: 50 MB (34.29%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -0,0 +1,23 @@
+const cache: {[n: number]: number} = {};
+
+function climbStairs(n: number): number {
+
+   if (n === 1 || n === 2)  {
+       return n;
+   }
+   let n_1: number, n_2: number;
+   if (cache[n-1]) {
+       n_1 = cache[n-1];
+    } else {
+        n_1 = climbStairs(n-1);
+        cache[n-1] = n_1;
+    }
+    if (cache[n-2]) {
+        n_2 = cache[n-2];
+    } else {
+        n_2 = climbStairs(n-2);
+        cache[n-2] = n_2;
+    }
+   
+   return n_1 + n_2;
+};
